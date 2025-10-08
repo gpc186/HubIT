06ec8c530b190e79826b915b22f3003f3c847eb1
gpc186 <gpc186@gmail.com>
2025-10-08 14:11:06 -0300

    Alterando caminho no servidor e também alterações no front end
    Co-authored-by: snchs <FernandoSnchs@users.noreply.github.com>
    Co-authored-by: Henrique Berdoldi Fiorotti <hberdoldifiorotti@gmail.com>

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.use('/assets', express.static('assets'));
 
 app.get('/', (req, asw)=>{
 	asw.status(200).sendFile(path.join(__dirname, 'public', 'login.html'));
-})
+});
+
+app.get('/contact', (req, asw)=>{
+	asw.status(200).sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
 
 app.listen(port, ()=>{
 	console.log(`Server is up and listening on port: ${port}`);
