b4f5b62bc1b315cca636bdbe4f06726eab4f697b
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2025-10-10 09:17:20 -0300

    Adicionando comentários explicativos no carregamento e tratamento de perfil de usuário

--- public/assets/js/perfil.js
+++ public/assets/js/perfil.js
@@ -1,3 +1,4 @@
+// Quando a página carregar: Rodar função carregarPerfil()
 window.onload = async function() {
     await carregarPerfil();
 };
@@ -9,6 +10,7 @@ async function carregarPerfil() {
     
     const idLogado = localStorage.getItem('userID');
     
+    // Verifica se o usuário já está logado
     if (!idLogado) {
         alert('Você precisa fazer login!');
         window.location.href = '/';
@@ -19,6 +21,7 @@ async function carregarPerfil() {
     
     try {
         
+        // Armazena o ID inserido em uma constante 
         const resposta = await fetch(`/api/usuario/${userID}`, {
             method: 'GET',
             headers: {
@@ -28,12 +31,14 @@ async function carregarPerfil() {
         
         const dados = await resposta.json();
         
+        // Verifica condição de sucesso e tratamento de erro
         if (resposta.ok) {
             alert(JSON.stringify(dados.usuario));
         } else {
             alert('Erro: ' + dados.error);            
         }
         
+    //Tratamento de erro de carregar o perfil
     } catch (error) {
         console.error(error);
         alert('Erro ao carregar perfil');
@@ -41,7 +46,10 @@ async function carregarPerfil() {
 }
 
 function sair() {
+    // Faz logout
     localStorage.clear();
     alert('Você saiu da conta');
+
+    // Volta para página inicial (root)
     window.location.href = '/';
 }
