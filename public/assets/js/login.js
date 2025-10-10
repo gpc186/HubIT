be021441c0002d27133e83ba34fb26f5cd0cb3f6
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2025-10-10 09:00:50 -0300

    Melhorando o tratamento de erros e comentários nas funções de registro e login de usuário

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,10 +1,14 @@
 function registrarNovoUsuario() {
+	// Criação de constantes de input e radio
 	const email = document.getElementById('emailNewUser').value;
 	const passwd = document.getElementById('passwdNewUser').value;
 	const usuario = document.getElementById('usuario').checked;
 	const empresa = document.getElementById('empresa').checked;
+
+	// radio de tipo da conta inativo, esperando ser selecionado
 	let tipoConta = false
     
+	// Verificador de condição para o tipo da conta
 	if (usuario) {
 		tipoConta = 'usuario'
 	}
@@ -12,21 +16,25 @@ function registrarNovoUsuario() {
 		tipoConta = 'empresa'
 	}
 
+	// Tratamento de erro caso nada seja inserido
 	if (!email || !passwd || !tipoConta) {
 		alert('Por favor preencha todos os campos!');
 		return;
 	};
 
+	// 'fetch' é uma forma de fazer 'curl' direto no código
 	fetch('/api/registrar', {
-		method: 'POST',
-		headers: {
+		// Parametros que seriam inseridos no 'curl' já são inseridos aqui
+		method: 'POST', // -X POST
+		headers: { // -H "Content-type: application/json"
 			'Content-Type': 'application/json'
 		},
-		body: JSON.stringify({ email, passwd, tipoConta })
+		body: JSON.stringify({ email, passwd, tipoConta }) // -d (Aqui é pego os dados inseridos nos inputs e transformados em um body para requisição)
 	})
 	.then(res => res.json())
 	.then(data => {
 
+		// Constante para pegar valor informado no Modal do Bootstrap
 		const modal = bootstrap.Modal.getInstance(
                 document.getElementById('exampleModal')
             );
@@ -38,25 +46,32 @@ function registrarNovoUsuario() {
 
 		if (data.success) {
 			alert('Usuário registrado com sucesso!');
-		} else {
+		} 
+		//Tratamento de erro em registrar o usuário
+		else {
 			alert('Erro ao registrar usuário: ' + (data.error || 'erro desconhecido'));
 		}
 	})
+	// Tratamento de erro
 	.catch(err => {
 		console.error(err);
 		alert('Erro de comunicação com o servidor');
 	});
 }
 
+// Fazer login
 function fazerLogin() {
+	// Constantes de inputs do Login de usuário
 	const email = document.getElementById('email').value;
 	const passwd = document.getElementById('passwd').value;
 	
+	// Verifica se foi inserido dados
 	if (!email || !passwd) {
 		alert('Preencha todos os campos!');
 		return;
 	};
 
+	// fetch de conteudos para o login
 	fetch('/api/login', {
 		method: 'POST',
 		headers: {
@@ -67,19 +82,26 @@ function fazerLogin() {
 	.then(res => res.json())
 	.then(data =>{
 		if(data.success){
+			// Constante que recebe o userID do user.json
 			const userID = data.usuario.userID;
 
+			// Aqui é armazenado "variáveis" com valores para serem vistos no JSON
 			localStorage.setItem('userID', userID);
 			localStorage.setItem('userEmail', data.usuario.email);
 			localStorage.setItem('userTipo', data.usuario.tipoConta);
 
+			// Redireciona o usuário logado para página dele
 			window.location.href = `/perfil/${userID}`;
-
 			alert('Usuario logado com sucesso!');
-		} else {
+
+		} 
+		
+		// Tratamento de erros ao logar
+		else {
 			alert('Erro ao logar o usuário: ' + (data.error || 'erro desconhecido'));
 		}
 	})
+	// Tratamento de erros
 	.catch(error =>{
 		console.error(error);
         alert('Erro ao carregar perfil');
